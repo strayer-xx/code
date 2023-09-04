70efde73075c9b11422c1d8ca1551526b4ee4077
LiuYingjie <liuyingjiess@gmail.com>
2023-09-04 14:23:31 +0800

    feat: 一些修改

--- src/main/java/com/yingjie/leetcode/dp/L0221.java
+++ src/main/java/com/yingjie/leetcode/dp/L0221.java
@@ -1,6 +1,8 @@
 package com.yingjie.leetcode.dp;
 
 /**
+ * 221. 最大正方形
+ *
  * 在一个由 '0' 和 '1' 组成的二维矩阵内，找到只包含 '1' 的最大正方形，并返回其面积。
  *
  * <a href="https://leetcode.cn/problems/maximal-square/">...</a>
@@ -10,7 +12,7 @@ public class L0221 {
 
 
     /**
-     * dp[i + 1][j + 1] 表示以第 i 行、第 j 列为右下角的正方形的最大边长
+     * dp[i][j] 表示以第 i 行、第 j 列为右下角的正方形的最大边长
      */
     public int maximalSquare(char[][] matrix) {
 
@@ -20,15 +22,19 @@ public class L0221 {
         int width = matrix[0].length;
         int maxSide = 0;
 
-        // 相当于已经预处理新增第一行、第一列均为0
-        int[][] dp = new int[height + 1][width + 1];
+        int[][] dp = new int[height][width];
 
         for (int row = 0; row < height; row++) {
             for (int col = 0; col < width; col++) {
                 if (matrix[row][col] == '1') {
-                    // 求dp[row + 1][col], dp[row][col + 1], dp[row][col]三者最小值
-                    dp[row + 1][col + 1] = Math.min(Math.min(dp[row + 1][col], dp[row][col + 1]), dp[row][col]) + 1;
-                    maxSide = Math.max(maxSide, dp[row + 1][col + 1]);
+                    if (row == 0 || col == 0) {
+                        dp[row][col] = 1;
+                    } else {
+                        // 求dp[row + 1][col], dp[row][col + 1], dp[row][col]三者最小值
+                        dp[row][col] = Math.min(Math.min(dp[row][col - 1], dp[row - 1][col]), dp[row - 1][col - 1]) + 1;
+                    }
+
+                    maxSide = Math.max(maxSide, dp[row][col]);
                 }
             }
         }
