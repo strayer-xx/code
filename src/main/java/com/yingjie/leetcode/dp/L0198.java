40943c302eb2a70c75fddcfd156553abe9a3456e
LiuYingjie <liuyingjiess@gmail.com>
2023-09-18 18:04:49 +0800

    feat: 一些修改

---
+++ src/main/java/com/yingjie/leetcode/dp/L0198.java
@@ -0,0 +1,101 @@
+package com.yingjie.leetcode.dp;
+
+/**
+ * <p>Title: L0198</p>
+ * <p>Description: 198. 打家劫舍</p>
+ *
+ * 你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，
+ * 如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+ *
+ * 给定一个代表每个房屋存放金额的非负整数数组，计算你 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。
+ *
+ * 输入：[1,2,3,1]
+ * 输出：4
+ * 解释：偷窃 1 号房屋 (金额 = 1) ，然后偷窃 3 号房屋 (金额 = 3)。
+ *      偷窃到的最高金额 = 1 + 3 = 4 。
+ *
+ * <a href="https://leetcode.cn/problems/house-robber/description/">...</a>
+ *
+ * Next: {@link L0213}
+ */
+public class L0198 {
+
+    public static void main(String[] args) {
+        new L0198().rob2(new int[]{1,2,3,1});
+    }
+
+    /**
+     * dp
+     */
+    public int rob(int[] nums) {
+        int[][] dp = new int[nums.length][2];
+        dp[0][0] = 0;
+        dp[0][1] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i][0] = Math.max(dp[i - 1][1], dp[i - 1][0]);
+            dp[i][1] = dp[i - 1][0] + nums[i];
+        }
+        return Math.max(dp[nums.length - 1][0], dp[nums.length - 1][1]);
+    }
+
+    /**
+     *
+     */
+    public int rob2(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int length = nums.length;
+        if (length == 1) {
+            return nums[0];
+        }
+        int[] dp = new int[length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < length; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[length - 1];
+    }
+
+    public int rob3(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int length = nums.length;
+        if (length == 1) {
+            return nums[0];
+        }
+        int first = nums[0], second = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < length; i++) {
+            int temp = second;
+            second = Math.max(first + nums[i], second);
+            first = temp;
+        }
+        return second;
+    }
+
+
+    /**
+     * dfs(timeout)
+     */
+    public int rob4(int[] nums) {
+        int sum = 0;
+        dfs(nums, 0, false, sum);
+        return max;
+    }
+
+    private int max = 0;
+    private void dfs(int[] nums, int index, boolean preState, int sum) {
+        if (index >= nums.length) {
+            max = Math.max(sum, max);
+            return;
+        }
+        if (preState) {
+            dfs(nums, index + 1, false, sum);
+        } else {
+            dfs(nums, index + 1, true, sum + nums[index]);
+            dfs(nums, index + 1, false, sum);
+        }
+    }
+}
---
+++ src/main/java/com/yingjie/leetcode/dp/L0337.java
@@ -0,0 +1,53 @@
+package com.yingjie.leetcode.dp;
+
+import sun.reflect.generics.tree.Tree;
+
+/**
+ * <p>Title: L0337</p>
+ * <p>Description: 337. 打家劫舍 III</p>
+ *
+ * 小偷又发现了一个新的可行窃的地区。这个地区只有一个入口，我们称之为 root 。
+ * 除了 root 之外，每栋房子有且只有一个“父“房子与之相连。一番侦察之后，聪明的小偷意识到“这个地方的所有房屋的排列类似于一棵二叉树”。
+ * 如果 两个直接相连的房子在同一天晚上被打劫 ，房屋将自动报警。
+ *
+ * 给定二叉树的 root 。返回 在不触动警报的情况下 ，小偷能够盗取的最高金额 。
+ */
+public class L0337 {
+
+    /**
+     * dp
+     */
+    public int rob(TreeNode root) {
+        return 0;
+    }
+
+
+    /**
+     * dfs
+     */
+    public int rob2(TreeNode root) {
+        return 1;
+    }
+
+    private void dfs(TreeNode root, int preStat) {
+        if (root == null) {
+            return;
+        }
+
+    }
+
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
