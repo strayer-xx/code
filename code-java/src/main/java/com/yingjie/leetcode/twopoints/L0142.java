ecb2bbf492fea111cdb196dc094290be38968454
liuyingjie <liuyingjie33@huawei.com>
2026-04-15 11:10:00 +0800

    L.

--- code-java/src/main/java/com/yingjie/leetcode/twopoints/L0142.java
+++ code-java/src/main/java/com/yingjie/leetcode/twopoints/L0142.java
@@ -1,6 +1,5 @@
 package com.yingjie.leetcode.twopoints;
 
-
 /**
  * 给定一个链表的头节点  head ，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
  *
@@ -13,7 +12,28 @@ package com.yingjie.leetcode.twopoints;
  * <a href="https://leetcode.cn/problems/linked-list-cycle-ii/description/">...</a>
  */
 public class L0142 {
-    
+
+    public ListNode detectCycle1(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        do {
+            if (fast.next == null || fast.next.next == null) {
+                return null;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        } while (slow != fast);
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+
     public ListNode detectCycle(ListNode head) {
         if (head == null) {
             return null;
@@ -46,10 +66,20 @@ public class L0142 {
 
     public static class ListNode {
         int val;
+
         ListNode next;
-        ListNode() {}
-        ListNode(int val) { this.val = val; }
-        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
     }
-    
+
 }
